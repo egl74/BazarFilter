36575fab1c9d284ec412a472e209d5e1f391470c
Jahor Lapcienok <jahor@jahors-mbp.zte.com.cn>
2017-12-14 22:17:03 +0300

    manya

---
+++ manyabot.rb
@@ -0,0 +1,27 @@
+#require 'unicode'
+require 'telegram/bot'
+
+token = '435455268:AAEyJrD-S1TeX5yIAzJEmzdUDoYskcHTfes'
+
+class Validator
+
+	WORDSTOFILTER = ["маня", "мань"].freeze
+	
+	def validateMessage(message)
+		WORDSTOFILTER.each do |word|
+			#if Unicode::downcase(message).include? word
+			if message.downcase.include? word
+			return false
+			end
+		end
+		return true
+	end
+end
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+  	unless Validator.new.validateMessage(message.text)
+  		bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, ты пидор!")
+  	end
+  end
+end
