3d26f28d62c4e44da8227d7bfb68a2c186ae2a0a
NilMV <nil.medvedev17@gmail.com>
2017-12-15 13:44:14 +0300

    refactored code + rebygemset and version

--- manyabot.rb
+++ manyabot.rb
@@ -1,26 +1,17 @@
 require 'unicode'
 require 'telegram/bot'
 
-token = '435455268:AAEyJrD-S1TeX5yIAzJEmzdUDoYskcHTfes'
+TOKEN = '435455268:AAEyJrD-S1TeX5yIAzJEmzdUDoYskcHTfes'
 
-class Validator
+WORDSTOFILTER = ["маня", "мань"].freeze
 
-	WORDSTOFILTER = ["маня", "мань"].freeze
-	
-	def validateMessage(message)
-		WORDSTOFILTER.each do |word|
-			if Unicode::downcase(message).include? word
-				return false
-			end
-		end
-		return true
-	end
-end
-
-Telegram::Bot::Client.run(token) do |bot|
+Telegram::Bot::Client.run(TOKEN) do |bot|
   bot.listen do |message|
-  	unless Validator.new.validateMessage(message.text)
-  		bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, ты пидор!")
-  	end
+    def validate_message(message)
+      WORDSTOFILTER.each do |word|
+        Unicode::downcase(message.text).include? word
+      end
+    end
+    bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name}, ты пидор!") if validate_message(message) 
   end
 end
