412bcba87ff4452a02211566f2ab261a367efc4a
Jahor Lapcienok <jahor@jahors-mbp.zte.com.cn>
2017-12-16 07:55:46 +0300

    Added Unicode support

--- manyabot.rb
+++ manyabot.rb
@@ -1,15 +1,14 @@
 require 'telegram/bot'
+require 'unicode'
 
 MANYAFILTER = ["маня", "мань"]
 VYNOSITE = ["выносите"]
 
 class Validator
-
 	def validateMessage(message, filterList)
 		filterList.each do |word|
-			# TODO: replace with solid downcase
 			message = removeRepetitiveSymbols(message)
-			if message.downcase.include? word
+			if Unicode::downcase(message).include? word
 				return false
 			end
 		end
@@ -25,12 +24,12 @@ Telegram::Bot::Client.run(ENV["TOKEN"]) do |bot|
   validator = Validator.new
   bot.listen do |message|
 		unless message.nil? or message.text.nil? then
-      	unless validator.validateMessage(message.text, MANYAFILTER)
-  				bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name} остынь, он не манька, он #нетакойкаквсе!!")
-        end
-      	unless validator.validateMessage(message.text, VYNOSITE)
-					bot.api.send_photo(chat_id: message.chat.id, photo: "https://i.imgur.com/ERaRFiD.jpg", disable_notification: true)
-        end
+    	unless validator.validateMessage(message.text, MANYAFILTER)
+  			bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name} остынь, он не манька, он #нетакойкаквсе!!")
+      end
+      unless validator.validateMessage(message.text, VYNOSITE)
+				bot.api.send_photo(chat_id: message.chat.id, photo: "https://i.imgur.com/ERaRFiD.jpg", disable_notification: true)
+      end
     end
   end
 end
