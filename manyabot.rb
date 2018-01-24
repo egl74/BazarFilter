d90e822cbf2cbc444dde0c1478b7b02cf1b5518f
Jahor Lapcienok <jahor@Jahors-MacBook-Pro.local>
2018-01-24 22:55:40 +0300

    add game-changing features

--- manyabot.rb
+++ manyabot.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'net/http'
 require 'telegram/bot'
 require 'unicode'
 
@@ -7,11 +9,17 @@ BOOBS = ["сиськи"]
 BUTTS = ["жопа"]
 
 class Validator
+  def handleMessage(message, keyWords, handler)
+    unless validateMessage(message, keyWords)
+      handler.call()
+    end
+  end
+
   def validateMessage(message, filterList)
     filterList.each do |word|
       message = removeRepetitiveSymbols(message)
       if Unicode::downcase(message).include? word
-	return false
+	      return false
       end
     end
     return true
@@ -21,18 +29,42 @@ class Validator
   end
 end
 
+class ReplyComposer
+  def sendEmergencyPicture(bot, chatId)
+    bot.api.send_photo(chat_id: chatId, photo: "https://i.imgur.com/ERaRFiD.jpg", disable_notification: true)
+  end
+
+  def replyManya(bot, message)
+    bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name} остынь, он не манька, он #нетакойкаквсе!!")
+  end
+
+  def sendBoobs(bot, chatId)
+    sendNude(bot, chatId, "boobs")
+  end
+
+  def sendButts(bot, chatId)
+    sendNude(bot, chatId, "butts")
+  end
+
+  def sendNude(bot, chatId, query)
+    apiUrl = "http://api.o#{query}.ru/#{query}/1/1/random"
+    result = JSON.parse(Net::HTTP.get(URI.parse(apiUrl)))
+    randomId = result[0]["id"]
+    bot.api.send_photo(chat_id: chatId, photo: "media.o#{query}.ru/#{query}/#{"%05d" % randomId}.jpg", disable_notification: true)
+  end
+end
+
 Telegram::Bot::Client.run(ENV["TOKEN"]) do |bot|
   validator = Validator.new
+  replyComposer = ReplyComposer.new
   bot.listen do |message|
     unless message.nil? or message.text.nil? then
-      unless validator.validateMessage(message.text, BOOBS)	     
-      end
-      unless validator.validateMessage(message.text, MANYAFILTER)
-        bot.api.send_message(chat_id: message.chat.id, text: "#{message.from.first_name} остынь, он не манька, он #нетакойкаквсе!!")
-      end
-      unless validator.validateMessage(message.text, VYNOSITE)
-	bot.api.send_photo(chat_id: message.chat.id, photo: "https://i.imgur.com/ERaRFiD.jpg", disable_notification: true)
-      end
+      text = message.text
+      chatId = message.chat.id
+      validator.handleMessage(text, BOOBS, Proc.new { replyComposer.sendBoobs(bot, chatId) })
+      validator.handleMessage(text, BUTTS, Proc.new { replyComposer.sendButts(bot, chatId) })
+      validator.handleMessage(text, MANYAFILTER, Proc.new { replyComposer.replyManya(bot, message) })
+      validator.handleMessage(text, VYNOSITE, Proc.new { replyComposer.sendEmergencyPicture(bot, chatId) })
     end
   end
  end
