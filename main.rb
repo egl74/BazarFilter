0ce0b4483e0d72a5a33e2e66703e4af25b47a82e
Jahor Lapcienok <jahor@Jahors-MacBook-Pro.local>
2018-02-06 23:36:37 +0300

    add stock features

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 require 'json'
 require 'net/http'
+require 'nokogiri'
+require 'open-uri'
 require 'telegram/bot'
 require 'unicode'
 
@@ -54,17 +56,59 @@ class ReplyComposer
   end
 end
 
+class StockWatcher
+  def getQuote(symbol, bot, chatId)
+    bot.api.send_message(chat_id: chatId, text: fetchQuote(symbol))
+  end
+
+  def fetchQuote(symbol)
+    quote = ""
+    diff = ""
+    url = "https://finance.yahoo.com/quote/#{symbol}USD=X"
+    begin
+      doc = Nokogiri::HTML(open(url))
+      sleep(1)
+      doc.xpath('//*[@id="quote-header-info"]/div[3]/div[1]/span/text()').each do |q|
+        quote = "#{sprintf('%.2f', q.to_s.delete(',').to_f)}$"
+      end
+      doc.xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span').each do |d|
+        diff = diff.concat(d)
+      end
+      dotIndexes = (0 ... diff.length).find_all { |i| diff[i, 1] == '.' }
+      diff = diff[0..dotIndexes[0] + 2] + diff[(dotIndexes[0] + 7)..dotIndexes[1] + 2] + diff[(dotIndexes[1] + 7)..diff.length]
+    rescue Exception => e  
+      puts e.message
+      return "error"
+    end
+    return composeReply(symbol, quote, diff)
+  end
+
+  def composeReply(symbol, quote, diff)
+    return "#{Unicode::upcase(symbol)}: #{quote} #{diff}"
+  end
+end
+
 Telegram::Bot::Client.run(ENV["TOKEN"]) do |bot|
   validator = Validator.new
   replyComposer = ReplyComposer.new
+  stockWatcher = StockWatcher.new
   bot.listen do |message|
     unless message.nil? or message.text.nil? then
       text = message.text
       chatId = message.chat.id
+      begin
+        if (text.start_with?('/')) then
+          atIndex = text.index('@')
+          symbolEndIndex = atIndex.nil? ? text.length : atIndex - 1
+
+          stockWatcher.getQuote(text[1..symbolEndIndex], bot, chatId)
+        end
+
       validator.handleMessage(text, BOOBS, Proc.new { replyComposer.sendBoobs(bot, chatId) })
       validator.handleMessage(text, BUTTS, Proc.new { replyComposer.sendButts(bot, chatId) })
       validator.handleMessage(text, MANYAFILTER, Proc.new { replyComposer.replyManya(bot, message) })
       validator.handleMessage(text, VYNOSITE, Proc.new { replyComposer.sendEmergencyPicture(bot, chatId) })
     end
-  end
- end
+  end    
+end
+end
